04819375fe9bc82af54297dfe4220b3a1d1b2b3f
ElMau201003 <72569167@continental.edu.pe>
2025-07-02 16:50:52 -0500

    Estilo y Logo

--- app/src/main/java/com/deliciaapp/adapter/CarritoAdapter.kt
+++ app/src/main/java/com/deliciaapp/adapter/CarritoAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.deliciaapp.R
@@ -19,7 +20,7 @@ class CarritoAdapter(
         val tvNombre: TextView = itemView.findViewById(R.id.tvNombreCarrito)
         val tvPrecio: TextView = itemView.findViewById(R.id.tvPrecioCarrito)
         val etCantidad: EditText = itemView.findViewById(R.id.etCantidad)
-        val btnEliminar: Button = itemView.findViewById(R.id.btnEliminar)
+        val btnEliminar: ImageButton = itemView.findViewById(R.id.btnEliminar)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
